f3ba95b79a7bf9c7f0e999d6947519203f40f90b
mr-possible <sambhavd4@gmail.com>
2023-12-19 17:00:26 +0000

    Added `couponCode' as transient field in Product model

--- product-service/src/main/java/com/sambhav/productservice/models/Product.java
+++ product-service/src/main/java/com/sambhav/productservice/models/Product.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -18,4 +19,10 @@ public class Product {
     private String name;
     private String description;
     private BigDecimal price;
+
+    /**
+     * We don't need to save this field as we just needed that in the controller to send to coupon-service.
+     */
+    @Transient
+    private String couponCode;
 }

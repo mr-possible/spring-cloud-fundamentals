2214d1916b9b9507b7f39e85e538eb15b2f6c811
mr-possible <sambhavd4@gmail.com>
2023-12-19 16:04:21 +0000

    Registering coupon-service with Eureka server

--- coupon-service/src/main/java/com/sambhav/couponservice/CouponServiceApplication.java
+++ coupon-service/src/main/java/com/sambhav/couponservice/CouponServiceApplication.java
@@ -2,8 +2,10 @@ package com.sambhav.couponservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CouponServiceApplication {
 
 	public static void main(String[] args) {

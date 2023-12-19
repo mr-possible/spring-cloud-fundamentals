047eb749368c10ba16fbb4a451a8a73ebe5f49bb
mr-possible <sambhavd4@gmail.com>
2023-12-19 17:00:55 +0000

    Modified create-product rest endpoint to include feign client call

--- product-service/src/main/java/com/sambhav/productservice/controllers/ProductController.java
+++ product-service/src/main/java/com/sambhav/productservice/controllers/ProductController.java
@@ -1,7 +1,9 @@
 package com.sambhav.productservice.controllers;
 
+import com.sambhav.productservice.dto.Coupon;
 import com.sambhav.productservice.models.Product;
 import com.sambhav.productservice.repository.ProductRepository;
+import com.sambhav.productservice.restclients.CouponClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +16,13 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private CouponClient couponClient;
+
     @RequestMapping(value = "create", method = RequestMethod.POST)
     public Product create(@RequestBody Product product) {
+        Coupon coupon = couponClient.getCoupon(product.getCouponCode().trim());
+        product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
         return productRepository.save(product);
     }
 }

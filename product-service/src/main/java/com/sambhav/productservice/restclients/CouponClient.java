82d14df7eb0263aa37a339b1a29f67d2e103a4dc
mr-possible <sambhavd4@gmail.com>
2023-12-19 16:59:45 +0000

    Added coupon client

---
+++ product-service/src/main/java/com/sambhav/productservice/restclients/CouponClient.java
@@ -0,0 +1,14 @@
+package com.sambhav.productservice.restclients;
+
+import com.sambhav.productservice.dto.Coupon;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient("COUPON-SERVICE")
+public interface CouponClient {
+    @GetMapping("coupon/get/{couponCode}")
+    Coupon getCoupon(@PathVariable("couponCode") String couponCode);
+}

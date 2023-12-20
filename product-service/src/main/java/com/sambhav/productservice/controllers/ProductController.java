b34ab205df97c6df7195f42f9373cfde90463e3a
mr-possible <sambhavd4@gmail.com>
2023-12-20 17:30:58 +0000

    Setup fault-tolerance for create-product

--- product-service/src/main/java/com/sambhav/productservice/controllers/ProductController.java
+++ product-service/src/main/java/com/sambhav/productservice/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.sambhav.productservice.dto.Coupon;
 import com.sambhav.productservice.models.Product;
 import com.sambhav.productservice.repository.ProductRepository;
 import com.sambhav.productservice.restclients.CouponClient;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +21,16 @@ public class ProductController {
     private CouponClient couponClient;
 
     @RequestMapping(value = "create", method = RequestMethod.POST)
+    @Retry(name = "product-create-retry", fallbackMethod = "handleProductCreateError")
     public Product create(@RequestBody Product product) {
         Coupon coupon = couponClient.getCoupon(product.getCouponCode().trim());
         product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
         return productRepository.save(product);
     }
+
+    public Product handleProductCreateError(Product product, Exception exception) {
+        System.out.println("Coupon-service seems to be unavailable at the moment! Try again later...");
+        return product;
+    }
+
 }
